Add slight fixes to gradient descent

src/main.py
@@ -3,24 +3,24 @@ import napari
 from napari.qt import thread_worker
 from qtpy.QtWidgets import QMessageBox
 #from src.segmentation.segmentation import segment
-#from src.slice_select.optimization import get_optimal_slice
-#from src.uncertainty.uncertainty import calculate_uncertainty_fields
+from src.slice_select.optimization import get_optimal_slice
+from src.uncertainty.uncertainty import calculate_uncertainty_fields
 from process_patients import *
 
 
-#folder_path = "D:/RP/RPData/PDDCA-1.4.1_part1" # replace with the path of your folder
-folders = ["D:/RP/RPData/PDDCA-1.4.1_part1", "D:/RP/RPData/PDDCA-1.4.1_part2", "D:/RP/RPData/PDDCA-1.4.1_part3"]
-folder_names = []
-
-# loop over all files in the folder
-for folder_path in folders:
-    for item in os.listdir(folder_path):
-        # check if the item is a directory
-        if os.path.isdir(os.path.join(folder_path, item)):
-            folder_names.append(item)
+# #folder_path = "D:/RP/RPData/PDDCA-1.4.1_part1" # replace with the path of your folder
+# folders = ["D:/RP/RPData/PDDCA-1.4.1_part1", "D:/RP/RPData/PDDCA-1.4.1_part2", "D:/RP/RPData/PDDCA-1.4.1_part3"]
+# folder_names = []
+#
+# # loop over all files in the folder
+# for folder_path in folders:
+#     for item in os.listdir(folder_path):
+#         # check if the item is a directory
+#         if os.path.isdir(os.path.join(folder_path, item)):
+#             folder_names.append(item)
 
 def load_image(viewer, filename):
-    patient_dest_dir = Path("D:/RP/UsedData/{}".format(filename))
+    patient_dest_dir = Path("C:/Users/jonas/Documents/git/Medical-Image-Processing/UsedData/{}".format(filename))
     img = np.load(patient_dest_dir.joinpath("img.npy"))
     print(img.shape)
     print(img)
@@ -42,7 +42,7 @@ def confirm_dialog(title, message):
 # load 3D image (proxies)
 # The subset here is manually input in the code, we could
 set = ['0522c0708', '0522c0195', '0522c0479']
-#convert_to_numpy("D:/RP/RPData" , "D:/RP/UsedData", subset=set)
+#convert_to_numpy("C:/Users/jonas/Documents/git/Medical-Image-Processing/RPData", "C:/Users/jonas/Documents/git/Medical-Image-Processing/UsedData", subset=set)
 
 # create a napari viewer
 viewer = napari.Viewer()
@@ -62,13 +62,15 @@ def user_check(viewer):
     confirmed = confirm_dialog("Process Status", "Is this image sufficiently segmented?")
     # load the new image if the user confirmed
     if confirmed:
-        # TODO: So here the user has accepted the segemtnation, what do we do with this?
+        # TODO: So here the user has accepted the segmentation, what do we do with this?
         #napari.quit()
         print("OVER")
     else:
         print("do slice selection")
+        uncertainty_field = calculate_uncertainty_fields()
         # find optimal slice
-        # load the slice as a special imaage with a name n
+        uncertainty, point, normal = get_optimal_slice(uncertainty_field)
+        # load the slice as a special image with a name n
 
 @viewer.bind_key('c')
 def user_input(viewer):

src/slice_select/optimization.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2 as cv2
+from scipy.ndimage import rotate
 
 # takes the uncertainty field and returns the plane (slice) with the highest uncertainty
 
@@ -42,22 +43,34 @@ best_of_planes = []
 # def calculate(pos):
 #     return 0
 
+def optimization():
+    return
 
-def method(uncertainty):
+def get_optimal_slice(uncertainty):
     x, _, _ = uncertainty.shape
     highest_value = -1
     highest_x = -1
     step_size = 1
     v = -1
     for i in range(0, 1):
-        # x = np.random.rand(1) * x
+        # Random Point on the plane
         x = int(np.random.uniform(1, x - 1))
+
+        # # Random normal
+        # normal = np.random.uniform(0, 1, 3)
+        # normal = normal / np.sum(normal)
+        #
+        # # Rotate the uncertainity according to normal
+        # plane = arr[:, :, x] + np.outer(normal, np.arange(arr.shape[0])) + np.outer(np.arange(arr.shape[1]), normal)
+
         temp_x, temp, temp_v = gradient_descent2(uncertainty, x, step_size)
         if highest_value < temp:
             highest_value = temp
             highest_x = temp_x
             v = temp_v
-    return highest_value, highest_x, v
+
+    return uncertainty[highest_x], highest_x, normal
+    #return highest_value, highest_x, v
 
 
 def gradient_descent2(uncertainty, x, step_size):
@@ -77,6 +90,12 @@ def func(plane):
     arr = abs(gradient_image * plane)
     return np.sum(arr)
 
+def rotate(normal):
+    # Compute the rotation angle and axis
+    angle = np.arccos(normal[0])
+    # Axis: (1,0,0) = (1,2), (0,1,0) = (0,2), (0,0,1) = (0,1)
+    rotated_arr = np.transpose(np.roll(arr, -int(np.degrees(angle)), axis=(1, 2)), axes=(0, 2, 1))
+    return rotated_arr
 
 if __name__ == "__main__":
     # arr = np.arange(20)
@@ -88,20 +107,31 @@ if __name__ == "__main__":
     # test_arr = test_arr + noise
     # np.save('my_array.npy', test_arr)
     test_arr = np.load('my_array.npy')
-
+    arr = np.load('my_array.npy')
     print("Mean: " + str(np.mean(test_arr, (1, 2))))
 
-    arr = []
+    x = int(np.random.uniform(1, 19))
 
-    for x in range(1, 20):
-        arr.append(func(test_arr[x]))
+    # Random normal
+    normal = np.random.uniform(0, 1, 3)
+    normal = normal / np.sum(normal)
+    print(arr.shape)
+    # Rotate the uncertainty according to normal
+    plane = arr[:, :, x] + np.outer(normal, np.arange(arr.shape[0])) + np.outer(np.arange(arr.shape[1]), normal)
 
-    print(arr)
+    print(plane)
 
-    h_v, h_x, v = method(test_arr)
-    print(h_v)
-    print(h_x)
-    print(v)
+    # arr = []
+    #
+    # for x in range(1, 20):
+    #     arr.append(func(test_arr[x]))
+    #
+    # print(arr)
+    #
+    # h_v, h_x, v = method(test_arr)
+    # print(h_v)
+    # print(h_x)
+    # print(v)
     # print(test_arr[h_x + 1])
     # print(test_arr[h_x - 1])
     # print(np.sum(test_arr[1]))