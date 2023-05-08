Fix the gradient descent.

src/main.py
@@ -22,8 +22,11 @@ for folder_path in folders:
 def load_image(viewer, filename):
     patient_dest_dir = Path("D:/RP/UsedData/{}".format(filename))
     img = np.load(patient_dest_dir.joinpath("img.npy"))
+    print(img.shape)
+    print(img)
     structures = np.load(patient_dest_dir.joinpath("structures.npy")).astype(int)
-
+    print(structures.shape)
+    print(structures)
     img_layer = viewer.add_image(img, name="CT scan", colormap="gray", interpolation2d="bicubic")
     seg_layer = viewer.add_labels(structures, name="segmentation")
     return (img_layer, seg_layer)
@@ -77,34 +80,4 @@ def user_input(viewer):
 
 # start the napari event loop
 napari.run()
-
-
-
-# # init user interface
-# # TODO
-#
-# # load 3D image from selection
-# # TODO
-#
-# isAccepted = False
-#
-# # while not isAccepted:
-# #
-# #     # segment (caution: first time and post-user-input may differ)
-# #     #segment()
-# #
-# #     # calculate uncertainty fields
-# #     #calculate_uncertainty_fields()
-# #
-# #     # decide if the segmentation is good enough
-# #     # TODO
-# #
-# #     if isAccepted:
-# #         break
-# #
-# #     # get the most uncertain slice for user input
-# #     #get_optimal_slice()
-# #
-# #     # present to user and receive changes
-# #     # TODO
 

src/slice_select/optimization.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2 as cv2
+
 # takes the uncertainty field and returns the plane (slice) with the highest uncertainty
 
 
@@ -48,35 +49,33 @@ def method(uncertainty):
     highest_x = -1
     step_size = 1
     v = -1
-    for i in range(0,1):
-        #x = np.random.rand(1) * x
-        x = int(np.random.uniform(5 , x - 5))
-        temp_x,temp,temp_v = gradient_descent2(uncertainty, x, step_size)
+    for i in range(0, 1):
+        # x = np.random.rand(1) * x
+        x = int(np.random.uniform(1, x - 1))
+        temp_x, temp, temp_v = gradient_descent2(uncertainty, x, step_size)
         if highest_value < temp:
             highest_value = temp
             highest_x = temp_x
             v = temp_v
     return highest_value, highest_x, v
 
+
 def gradient_descent2(uncertainty, x, step_size):
     current_pos = x
     for i in range(250):
-        print(current_pos)
-        print(func(uncertainty[current_pos+1,:,:]))
-        gradient_difference = func(uncertainty[current_pos+2,:,:]) - func(uncertainty[current_pos+1,:,:])
-        print(gradient_difference)
+        #print(current_pos)
+        gradient_difference = func(uncertainty[current_pos + 2, :, :]) - func(uncertainty[current_pos + 1, :, :])
         if gradient_difference > 0:
             current_pos += step_size  # difference in position should or shouldn't be dependant on gradient?
         else:
             current_pos -= step_size
-    return current_pos, func(uncertainty[x, :, :]), x
+    return current_pos + 1, func(uncertainty[current_pos + 1, :, :]), x
 
 
 def func(plane):
-    gradient_image = cv2.Laplacian(plane,cv2.CV_64F)
-    return np.sum(gradient_image)
-
-
+    gradient_image = cv2.Laplacian(plane, cv2.CV_64F)
+    arr = abs(gradient_image * plane)
+    return np.sum(arr)
 
 
 if __name__ == "__main__":
@@ -90,11 +89,11 @@ if __name__ == "__main__":
     # np.save('my_array.npy', test_arr)
     test_arr = np.load('my_array.npy')
 
-    print("Mean: " + str(np.mean(test_arr, (1,2))))
+    print("Mean: " + str(np.mean(test_arr, (1, 2))))
 
     arr = []
 
-    for x in range(1,20):
+    for x in range(1, 20):
         arr.append(func(test_arr[x]))
 
     print(arr)
@@ -107,5 +106,3 @@ if __name__ == "__main__":
     # print(test_arr[h_x - 1])
     # print(np.sum(test_arr[1]))
     # print(np.sum(test_arr[4]))
-
-