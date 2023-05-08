47c1e9433ec9eb437a3965e79eb4ce5137568c33
Sungjin Kim <s.kim-12@student.tudelft.nl>
2023-05-08 21:04:06 +0200

    Finish baseline uncertainty (UNTESTED)

--- src/uncertainty/uncertainty.py
+++ src/uncertainty/uncertainty.py
@@ -1,6 +1,8 @@
 
 import math
 import numpy as np
+import numpy.linalg as nl
+import scipy.ndimage as sn
 
 # takes the output from segment() and returns a 3D-array with uncertainties [0-1]
 def calculate_uncertainty_fields(image, label, prob):
@@ -25,15 +27,24 @@ def calculate_uncertainty_fields(image, label, prob):
     # initialize output uncertainty field
     output_field = np.zeros((ix, iy, iz))
 
-    # to be used for calculations
+    # parameters for foreground & background distributions
     mean_fg, std_fg, mean_bg, std_bg = gaussian_foreground_background(image, label)
+
+    # distance maps (or transform)
+    label_inv = np.invert(label)
+    dist_from_0 = sn.distance_transform_edt(label)
+    dist_from_1 = sn.distance_transform_edt(label_inv)
+
+    # image gradient
+    dx, dy, dz = np.gradient(image)
 
     # calculate uncertainty for each voxel
     for x in range(0, ix):
         for y in range(0, iy):
             for z in range(0, iz):
                 u_e = entropy_energy(prob[x, y, z])
-                u_b = boundary_energy((x, y, z), label[x, y, z], image)
+                u_b = boundary_energy((x, y, z), label[x, y, z],
+                                      dist_from_0, dist_from_1, (dx, dy, dz))
                 u_r = regional_energy(image[x, y, z], label[x, y, z],
                                       mean_fg, std_fg, mean_bg, std_bg)
                 output_field[x, y, z] = 0.8 * u_e + 0.05 * u_b + 0.15 * u_r
@@ -49,21 +60,24 @@ def entropy_energy(x_prob):
 
 ###
 
-def boundary_energy(x_coord, label, image, alpha=2):
-    delta = soft_delta_func(distance_to_boundary(x_coord, label))
-    gradient_abs_pow = math.pow(abs(image_gradient(x_coord, image)), alpha)
+def boundary_energy(x_coord, label, df0, df1, gradient, alpha=2):
+    delta = soft_delta_func(distance_to_boundary(x_coord, label, df0, df1))
+    gradient_abs_pow = math.pow(voxel_norm(x_coord, gradient), alpha)
     return delta / (1 + gradient_abs_pow)
 
-def distance_to_boundary(x_coord, label):
-    # TODO - import or transcribe MATLAB.bwdist
-    distance_map = bwdist(label)
+def distance_to_boundary(x_coord, label, df0, df1):
+    x, y, z = x_coord
+    if label == 0:
+        # voxel is classified as 1, get distance from nearest 0
+        return df1[x, y, z]
+    else:
+        # voxel is classified as 0, get distance from nearest 1
+        return df0[x, y, z]
 
-    # TODO - calculate distance
-    return 0
-
-def image_gradient(x_coord, image):
-    # TODO - calculate image gradient
-    return 0
+def voxel_norm(x_coord, gradient):
+    x, y, z = x_coord
+    dx, dy, dz = gradient
+    return nl.norm(np.array([dx[x, y, z], dy[x, y, z], dz[x, y, z]]))
 
 def soft_delta_func(x):
     return math.pow(math.e, -(x * x) / 2)
