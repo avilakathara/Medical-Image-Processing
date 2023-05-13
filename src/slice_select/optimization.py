4f5dd75623b044d9d61689b0e1847a3554d7d02c
Jonas <J.vanMarrewijk-2@student.tudelft.nl>
2023-05-13 14:56:11 +0200

    Properly use gradient and step size and improve readability

--- src/slice_select/optimization.py
+++ src/slice_select/optimization.py
@@ -7,138 +7,53 @@ from scipy import ndimage
 from scipy.ndimage import rotate
 
 # takes the uncertainty field and returns the plane (slice) with the highest uncertainty
-
-
-best_of_planes = []
-
-
-# Input: 3D numpy array
-# Output: a 2D array representing the indexes in the 3D array
-
-# def get_optimal_slice():
-#     return
-#
-#
-# def multi_gradient_descent(uncertainty, iterations, step_size, multi):
-#     highest_value = -1
-#     for i in range(0, multi):
-#         normal = np.random.rand(3) * 360
-#         temp = gradient_descent(uncertainty, iterations, step_size, normal)
-#         if highest_value < temp:
-#             highest_value = temp
-#     return highest_value
-#
-#
-# # Input: a 3D numpy array with values between 0 and 1
-# def gradient_descent(uncertainty, iterations, step_size, normal):
-#     # instantiate random plane: random position within current segmentation
-#     current_pos = 56
-#     for i in range(iterations):
-#         # calculate gradient (uncertainty of one step above - one step below)
-#         gradient = calculate(current_pos+1) - calculate(current_pos-1)
-#         current_pos += gradient * step_size  # move to higher uncertainty, repeat
-#
-#     uncertainty_value = calculate(current_pos)
-#
-#     return uncertainty_value, current_pos
-#
-#
-# # Sum over a 2d array
-# def calculate(pos):
-#     return 0
-
 def optimization():
     return
 
 def get_optimal_slice(uncertainty):
     x, _, _ = uncertainty.shape
-    highest_value = -1
+    highest_uncertainty = -1
     highest_x = -1
-    step_size = 10
-    v = -1
+    step_size = 0.03
     normal = []
-    for i in range(0, 10):
-        # Random Point on the plane
-        x = int(np.random.uniform(1, x - 3))
+    for i in range(0, 1):
+        # Generate random point that, together with a normal, defines a plane
+        x = int(np.random.uniform(0, x))
 
-        # # Random normal
-        # normal = np.random.uniform(0, 1, 3)
-        # normal = normal / np.sum(normal)
-        #
-        # # Rotate the uncertainity according to normal
-        # plane = arr[:, :, x] + np.outer(normal, np.arange(arr.shape[0])) + np.outer(np.arange(arr.shape[1]), normal)
-
-        temp_x, temp, temp_v = gradient_descent3(uncertainty, x, step_size)
-        if highest_value < temp:
-            highest_value = temp
-            highest_x = temp_x
-            v = temp_v
+        # run gradient descent, starting at ax
+        current_x, current_uncertainty, _ = gradient_descent(uncertainty, x, step_size)
+        # compare gradient result with current best result
+        if highest_uncertainty < current_uncertainty:
+            highest_uncertainty = current_uncertainty
+            highest_x = current_x
 
     return uncertainty[highest_x], highest_x, normal, "x"
-    #return highest_value, highest_x, v
 
-# Currently using a very simple step size update i.e reduce over time
-def gradient_descent2(uncertainty, x, step_size):
+
+def gradient_descent(uncertainty, x, step_size):
     m, _, _ = uncertainty.shape
     #print(x)
     current_pos = x
-    for i in range(250):
-        #print(current_pos)
-        gradient_difference = func(uncertainty[current_pos + 2, :, :]) - func(uncertainty[current_pos + 1, :, :])
+    for i in range(40):
+        print("current position: " + str(current_pos))
 
-        gradient_difference_p = func(uncertainty[current_pos + 1, :, :]) - func(uncertainty[current_pos, :, :])
+        # prevent the current value from going outside of the allowed values
+        if current_pos <= 1:
+            current_pos = 2
+        if current_pos >= m - 1:
+            current_pos = m - 2
 
-        if gradient_difference >= 0 and gradient_difference_p >= 0:
-            if gradient_difference < gradient_difference_p:
-                step_size = int(step_size / 2)
-        if gradient_difference < 0 and gradient_difference_p < 0:
-            if abs(gradient_difference_p) < abs(gradient_difference):
-                step_size = int(step_size / 2)
-        if gradient_difference <= 0 and gradient_difference_p >= 0:
-            step_size = 1
-        if gradient_difference >= 0 and gradient_difference_p <= 0:
-            step_size = 1
-        #print(gradient_difference)
-        if gradient_difference > 0:
-            current_pos += step_size  # difference in position should or shouldn't be dependant on gradient?
-            if current_pos >= m:
-                current_pos = m-3
-        else:
-            current_pos -= step_size
-            if current_pos <= 0:
-                current_pos = 2
-    return current_pos + 1, func(uncertainty[current_pos + 1, :, :]), x
+        # gradient_difference is the gradient above the current plane, gradient_difference_p is the gradient below the
+        # current plane.
+        # [current_pos + 1] because in get optimal slice we start from index 1, meaning everything is shifted by 1.
+        gradient_difference = get_grad(uncertainty[current_pos + 1, :, :]) - get_grad(uncertainty[current_pos - 1, :, :])
 
-def gradient_descent3(uncertainty, x, step_size):
-    m, _, _ = uncertainty.shape
-    #print(x)
-    current_pos = x
-    for i in range(250):
-        #print(current_pos)
-        gradient_difference = get_grad(uncertainty[current_pos + 2, :, :]) - get_grad(uncertainty[current_pos + 1, :, :])
+        # gradient ascent
+        # TODO: Maybe make the step size reduction dynamic using i
+        print("current gradient: " + str(gradient_difference))
+        current_pos += int(gradient_difference * step_size)
 
-        gradient_difference_p = get_grad(uncertainty[current_pos + 1, :, :]) - get_grad(uncertainty[current_pos, :, :])
-
-        if gradient_difference >= 0 and gradient_difference_p >= 0:
-            if gradient_difference < gradient_difference_p:
-                step_size = int(step_size / 2)
-        if gradient_difference < 0 and gradient_difference_p < 0:
-            if abs(gradient_difference_p) < abs(gradient_difference):
-                step_size = int(step_size / 2)
-        if gradient_difference <= 0 and gradient_difference_p >= 0:
-            step_size = 1
-        if gradient_difference >= 0 and gradient_difference_p <= 0:
-            step_size = 1
-        #print(gradient_difference)
-        if gradient_difference > 0:
-            current_pos += step_size  # difference in position should or shouldn't be dependant on gradient?
-            if current_pos >= m:
-                current_pos = m-3
-        else:
-            current_pos -= step_size
-            if current_pos <= 0:
-                current_pos = 2
-    return current_pos + 1, get_grad(uncertainty[current_pos + 1, :, :]), x
+    return current_pos, get_grad(uncertainty[current_pos, :, :]), x
 
 def func(plane):
     gradient_image = cv2.Laplacian(plane, cv2.CV_64F)
@@ -182,8 +97,7 @@ if __name__ == "__main__":
     # test_arr = test_arr + noise
     # np.save('my_array.npy', test_arr)
     test_arr = np.load('my_array.npy')
-    arr = np.load('my_array.npy')
-    print("Mean: " + str(np.mean(test_arr, (1, 2))))
+    print("Mean values of planes: " + str(np.mean(test_arr, (1, 2))))
     # x = int(np.random.uniform(1, 19))
     #
     # # Random normal
