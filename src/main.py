f6829099c4473a3a842fefe88a512fac6e0d1ae0
Arjun <arjun.haridas6601@gmail.com>
2023-05-29 01:17:16 +0200

    Add discreet slice selection

--- src/main.py
+++ src/main.py
@@ -14,6 +14,7 @@ from uncertainty.uncertainty import calculate_uncertainty_fields
 
 from pathlib import Path
 
+
 def confirm_dialog(title, message):
     msg_box = QMessageBox()
     msg_box.setWindowTitle(title)
@@ -38,7 +39,6 @@ img = np.load(folder_path + str("/") + os.listdir(folder_path)[0])
 structures = np.load(folder_path + str("/") + os.listdir(folder_path)[1]).astype(int)
 ground_truth = np.zeros(structures.shape).astype(int)
 
-
 # CHOOSE ORGAN
 # 1=BrainStem,2=Chiasm,3=Mandible,4=OpticNerve_L,5=OpticNerve_R,6=Parotid_L,7=Parotid_R,8=Submandibular_L,9=Submandibular_R)
 organ_choice = 3
@@ -53,15 +53,15 @@ ymin, ymax = np.where(y)[0][[0, -1]]
 xmin, xmax = np.where(x)[0][[0, -1]]
 z_offset = 2
 xy_offset = 10
-zmin = max(0,zmin-z_offset)
-ymin = max(0,ymin-xy_offset)
-xmin = max(0,xmin-xy_offset)
-zmax = min(len(img),zmax+z_offset+1)
-ymax = min(len(img[0]),ymax+xy_offset+1)
-xmax = min(len(img[0][0]),xmax+xy_offset+1)
+zmin = max(0, zmin - z_offset)
+ymin = max(0, ymin - xy_offset)
+xmin = max(0, xmin - xy_offset)
+zmax = min(len(img), zmax + z_offset + 1)
+ymax = min(len(img[0]), ymax + xy_offset + 1)
+xmax = min(len(img[0][0]), xmax + xy_offset + 1)
 
-img = img[zmin:zmax,ymin:ymax,xmin:xmax]
-ground_truth = ground_truth[zmin:zmax,ymin:ymax,xmin:xmax]
+img = img[zmin:zmax, ymin:ymax, xmin:xmax]
+ground_truth = ground_truth[zmin:zmax, ymin:ymax, xmin:xmax]
 
 # SHOW IMAGES
 viewer.add_image(img, name="CT_SCAN", colormap="gray", interpolation2d="bicubic")
@@ -86,13 +86,14 @@ fetched_plane_index = None
 
 iterations = 0
 
+
 def create_contours(viewer):
     global livewire
     global click_count
     global seed_points
     global contours
     global lw_layer
-    global mypath # test shit
+    global mypath  # test shit
     global simulate_user_input
     global ground_truth
     global iterations
@@ -112,7 +113,7 @@ def create_contours(viewer):
         else:
             contours = automatic_contours(ground_truth)
             # np.save("contours", contours)
-            lw_layer = viewer.add_labels(contours, name='contours (automatic)',opacity=1.0)
+            lw_layer = viewer.add_labels(contours, name='contours (automatic)', opacity=1.0)
     else:
         livewire = LiveWire2(img, sigma=5.0)
         lw_layer = viewer.add_labels(livewire.drawing,
@@ -131,25 +132,25 @@ def create_contours(viewer):
             return
         if livewire.current_slice == -1:
             livewire.current_slice = round(event.position[0])
-            livewire.laplacian = abs(cv.Laplacian(img[livewire.current_slice],cv.CV_32F))
+            livewire.laplacian = abs(cv.Laplacian(img[livewire.current_slice], cv.CV_32F))
             livewire.max_gradient = np.max(livewire.laplacian)
         if livewire.current_slice != round(event.position[0]):
             click_count = 0
             livewire.reset()
             livewire.current_slice = round(event.position[0])
 
-            livewire.laplacian = abs(cv.Laplacian(img[livewire.current_slice],cv.CV_32F))
+            livewire.laplacian = abs(cv.Laplacian(img[livewire.current_slice], cv.CV_32F))
             livewire.max_gradient = np.max(livewire.laplacian)
 
         coords = event.position[1:]
         if valid(coords):
-            indices_start = flat_to_indices(livewire.start,img.shape[2])
-            dist_to_start = np.sqrt(((coords[0]-indices_start[0])**2) + ((coords[1]-indices_start[1])**2))
+            indices_start = flat_to_indices(livewire.start, img.shape[2])
+            dist_to_start = np.sqrt(((coords[0] - indices_start[0]) ** 2) + ((coords[1] - indices_start[1]) ** 2))
             if click_count > 2 and dist_to_start < 10:
                 # close contour if close to start
                 livewire.select(livewire.start)
             else:
-                livewire.select(flatten_indices([round(coords[0]),round(coords[1])],img.shape[2]))
+                livewire.select(flatten_indices([round(coords[0]), round(coords[1])], img.shape[2]))
             layer.data = livewire.current_drawing + livewire.drawing
 
     @lw_layer.mouse_drag_callbacks.append
@@ -162,6 +163,7 @@ def create_contours(viewer):
 
         click_count += 1
 
+
 def get_segmentation(viewer):
     global segmentation
     global probabilities
@@ -173,12 +175,15 @@ def get_segmentation(viewer):
     segmentation, probabilities = segment(img, seed_points)
     viewer.add_labels(segmentation, name="Segmentation {}".format(iterations))
 
+
 def get_uncertainty_field(viewer, draw=False):
     global uncertainty_field
     uncertainty_field = calculate_uncertainty_fields(img, segmentation, probabilities)
 
     if draw:
-        viewer.add_image(uncertainty_field, name="uncertainty_{}".format("u"), colormap="gray", interpolation2d="bicubic")
+        viewer.add_image(uncertainty_field, name="uncertainty_{}".format("u"), colormap="gray",
+                         interpolation2d="bicubic")
+
 
 def user_check(viewer):
     global fetched_plane_index
@@ -209,6 +214,7 @@ def user_check(viewer):
     with open('data.json', 'w') as f:
         json.dump(loaded_data, f)
 
+
 # MAKE ANNOTATIONS
 @viewer.bind_key('a')
 def on_press_a(viewer):
@@ -218,6 +224,18 @@ def on_press_a(viewer):
         pass
     create_contours(viewer)
 
+
+
+
+@viewer.bind_key('z')
+def test(viewer):
+    global img
+    roti = rot(img, [0,0,1], 45)
+    viewer.add_image(roti, name="rotated 45", colormap="gray", interpolation2d="bicubic")
+    roti = rot(img, [0, 0, 1], 315)
+    viewer.add_image(roti, name="rotated -45", colormap="gray", interpolation2d="bicubic")
+
+
 # MAKE SEGMENTATION
 @viewer.bind_key('s')
 def on_press_s(viewer):
@@ -226,6 +244,7 @@ def on_press_s(viewer):
     get_segmentation(viewer)
     get_uncertainty_field(viewer)
     user_check(viewer)
+
 
 @viewer.bind_key('c')
 def user_input(viewer):
@@ -255,6 +274,7 @@ def user_input(viewer):
             viewer.layers.remove(layer)
 
     # TODO: The modified slice has been gotten, we do something with this
+
 
 # INITIATE
 napari.run()
@@ -287,5 +307,3 @@ napari.run()
 #             for y in range(int(min(line[0][2],line[1][2])), int(min(line[0][2],line[1][2]))):
 #                 all_points.append([int(line[0][0]),int(line[0][1]),y])
 #     return all_points
-
-
---
+++ src/slice_select/rotation_methods.py
@@ -0,0 +1,60 @@
+import math
+import numpy as np
+from scipy.ndimage import affine_transform
+
+
+def rotate(image, axis, angle_deg, cval=0.0):
+    # Define the rotation axis and angle
+    axis = np.array(axis)  # Example: Rotate around the X-axis
+
+    # Convert the angle to radians
+    angle_rad = np.radians(angle_deg)
+
+    # Calculate the rotation matrix
+    cos_theta = np.cos(angle_rad)
+    sin_theta = np.sin(angle_rad)
+    u = axis / np.linalg.norm(axis)
+    rotation_matrix = np.array([
+        [cos_theta + u[0] * u[0] * (1 - cos_theta), u[0] * u[1] * (1 - cos_theta) - u[2] * sin_theta,
+         u[0] * u[2] * (1 - cos_theta) + u[1] * sin_theta],
+        [u[1] * u[0] * (1 - cos_theta) + u[2] * sin_theta, cos_theta + u[1] * u[1] * (1 - cos_theta),
+         u[1] * u[2] * (1 - cos_theta) - u[0] * sin_theta],
+        [u[2] * u[0] * (1 - cos_theta) - u[1] * sin_theta, u[2] * u[1] * (1 - cos_theta) + u[0] * sin_theta,
+         cos_theta + u[2] * u[2] * (1 - cos_theta)]
+    ])
+
+    # Define the center point for rotation
+    center = np.array(image.shape) / 2.0
+    translation = center - np.dot(rotation_matrix, center)
+    # Perform the 3D rotation
+    rotated_image = affine_transform(image, rotation_matrix, offset=translation, order=1, mode='constant', cval=cval)
+
+    return rotated_image
+
+
+def angle_between_vectors(vec1, vec2):
+    dot_product = sum(a * b for a, b in zip(vec1, vec2))
+    magnitude1 = math.sqrt(sum(a ** 2 for a in vec1))
+    magnitude2 = math.sqrt(sum(b ** 2 for b in vec2))
+    cosine_angle = dot_product / (magnitude1 * magnitude2)
+    angle_rad = math.acos(cosine_angle)
+    angle_deg = math.degrees(angle_rad)
+    return angle_deg
+
+
+def image_rotate(image, normal):
+    axis = np.array([1, 0, 0])
+    angle = angle_between_vectors(axis)
+    if angle > 5:
+        rotated_image = rotate(image, axis, angle)
+    axis = np.array([0, 1, 0])
+    angle = angle_between_vectors(axis)
+    if angle > 5:
+        rotated_image = rotate(rotated_image, axis, angle)
+    axis = np.array([0, 0, 1])
+    angle = angle_between_vectors(axis)
+    if angle > 5:
+        rotated_image = rotate(rotated_image, axis, angle)
+
+    return rotated_image
+
