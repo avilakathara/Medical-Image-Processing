Fix all rotation methods. Add dice coefficient calculation in main.

src/main.py
@@ -75,6 +75,14 @@ axis = None
 chosen_layer = None
 
 
+sim = 1
+
+def dice_coefficient(mask1, mask2):
+    intersection = np.sum(mask1 * mask2)
+    total = np.sum(mask1) + np.sum(mask2)
+    dice = (2.0 * intersection) / total
+    return dice
+
 def create_contours(viewer):
     global seed_points
     global contours
@@ -106,16 +114,23 @@ def create_contours(viewer):
         elif axis == "z":
             contours = create_contour(ground_truth[:, :, point])
         else:
+            ground_truth, pad, shape = true_img_rot(ground_truth, normal)
+            print("shape of gt is {}".format(ground_truth.shape))
             contours = create_contour(ground_truth[point])
+            ground_truth = true_img_rot_back(ground_truth, normal, pad, shape)
         contours_display = np.full(ground_truth.shape, False)
         if axis == "x":
             contours_display[point] = contours
+            contours_display, pad, shape = true_img_rot(contours_display, normal)
+            contours_display = true_img_rot_back(contours_display, normal, pad, shape)
         elif axis == "y":
             contours_display[:, point, :] = contours
         elif axis == "z":
             contours_display[:, :, point] = contours
         else:
-            contours_display[point] = contours
+            contours_display, pad, shape = true_img_rot(contours_display, normal)
+            # contours_display[point] = contours
+            # contours_display = true_img_rot_back(contours_display, normal, pad, shape)
         # np.save("contours", contours)
         lw_layer = viewer.add_labels(contours_display, name='INPUT {}'.format(iterations), opacity=1.0)
     else:
@@ -138,13 +153,13 @@ def get_segmentation(viewer):
         seed_points = convert_to_labels(contours)
     else:
         new_labeled_slice = convert_to_labels2d(contours)
-        if axis == "d1":
-            # rotated_ground_truth = rotate(ground_truth, [0, 0, 1], 315)
-            rotate_seed_points = rotate(seed_points, normal, 45)
-            rotate_seed_points[point] = new_labeled_slice
-            seed_points = rotate(rotate_seed_points, normal, 315)
-            viewer.add_labels(seed_points, name="seedpoints debug".format(iterations))
-        elif axis == "x":
+        # if axis == "d1":
+        #     # rotated_ground_truth = rotate(ground_truth, [0, 0, 1], 315)
+        #     rotate_seed_points = rotate(seed_points, normal, 45)
+        #     rotate_seed_points[point] = new_labeled_slice
+        #     seed_points = rotate(rotate_seed_points, normal, 315)
+        #     viewer.add_labels(seed_points, name="seedpoints debug".format(iterations))
+        if axis == "x":
             print(seed_points.shape)
             seed_points[point] = new_labeled_slice
         elif axis == "y":
@@ -152,13 +167,13 @@ def get_segmentation(viewer):
         elif axis == "z":
             seed_points[:, :, point] = new_labeled_slice
         else:
-            rotate_seed_points = true_img_rot(seed_points, normal)
+            rotate_seed_points, pad, shape = true_img_rot(seed_points, normal)
             rotate_seed_points[point] = new_labeled_slice
-            seed_points = true_img_rot_back(rotate_seed_points, normal)
+            seed_points = true_img_rot_back(rotate_seed_points, normal, pad, shape)
 
     segmentation, probabilities = segment(img, seed_points)
     seg_layer = viewer.add_labels(segmentation, name="Segmentation {}".format(iterations))
-
+    print("Dice coeff is: {}".format(dice_coefficient(segmentation, ground_truth)))
 
 def get_uncertainty_field(viewer, draw=False):
     global uncertainty_field
@@ -180,7 +195,7 @@ def user_check(viewer, discrete=True):
 
     # Find optimal slice
     # uncertainty, point, normal, chosen_axis = get_optimal_slice(uncertainty_field)
-    uncertainty, point, normal, chosen_axis = discreet_get_optimal_slice(uncertainty_field, True, True, True)
+    uncertainty, point, normal, chosen_axis = discreet_get_optimal_slice(uncertainty_field, False, False, False, True)
     axis = chosen_axis
 
     print("Iteration {} - MAX UNCERTAINTY at plane {} = {}".format(iterations, chosen_axis, point))
@@ -192,8 +207,8 @@ def user_check(viewer, discrete=True):
     elif chosen_axis == 'z' and discrete:
         chosen_slice = img[:, :, point]
     elif chosen_axis == "d1" and discrete:
-        image_unrotate = true_img_rot(img, [0, 0, 1])
-        chosen_slice = image_unrotate[point]
+        image_rot, pad, shape = true_img_rot(img, normal)
+        chosen_slice = image_rot[point]
 
     chosen_layer = viewer.add_image(chosen_slice, name="chosen_slice", colormap="gray", interpolation2d="bicubic")
 
@@ -220,10 +235,12 @@ def on_press_a(viewer):
 @viewer.bind_key('z')
 def test(viewer):
     global img
-    roti = true_img_rot(img, [0, 0, 1])
+    roti, pad, shape = true_img_rot(img, [0, 0, 1])
     viewer.add_image(roti, name="rotated 45", colormap="gray", interpolation2d="bicubic")
-    roti = true_img_rot_back(roti, [0, 0, 1])
-    viewer.add_image(roti, name="rotated -45", colormap="gray", interpolation2d="bicubic")
+    roti = true_img_rot_back(roti, [0, 0, 1], pad, shape)
+    viewer.add_image(roti, name="unrotated 45", colormap="gray", interpolation2d="bicubic")
+    # roti = true_img_rot_back(roti, [0, 0, 1])
+    # viewer.add_image(roti, name="rotated -45", colormap="gray", interpolation2d="bicubic")
 
 
 # MAKE SEGMENTATION
@@ -282,3 +299,30 @@ def on_press_s(viewer):
 
 # INITIATE
 napari.run()
+
+while(sim > 0):
+    try:
+        viewer.layers.remove(lw_layer)
+    except:
+        pass
+    create_contours(viewer)
+    try:
+        viewer.layers.remove(lw_layer)
+    except:
+        pass
+    try:
+        viewer.layers.remove(seg_layer)
+    except:
+        pass
+    try:
+        viewer.layer.remove(chosen_layer)
+    except:
+        pass
+
+    iterations += 1
+    get_segmentation(viewer)
+    get_uncertainty_field(viewer)
+    # print(evaluate_uncertainty(ground_truth, segmentation, uncertainty_field))
+    user_check(viewer)
+
+    sim -= 1

src/slice_select/discreet_optimization.py
@@ -4,15 +4,16 @@ import numpy as np
 import cv2 as cv2
 import matplotlib.pyplot as plt
 
-
 # from scipy import ndimage
 # from scipy.ndimage import rotate
 from slice_select.rotation_methods import rotate
 
+from src.slice_select.rotation_methods import true_img_rot
+
 
 def discreet_get_optimal_slice(uncertainty, x_axis=False, y_axis=False, z_axis=False, diagonal_1=False,
-                      diagonal_2=False, diagonal_3=False, diagonal_4=False, diagonal_5=False,
-                      diagonal_6=False):
+                               diagonal_2=False, diagonal_3=False, diagonal_4=False, diagonal_5=False,
+                               diagonal_6=False):
     print("Fetching most uncertain slice...")
 
     current_maximum_uncertainity = -1
@@ -23,9 +24,10 @@ def discreet_get_optimal_slice(uncertainty, x_axis=False, y_axis=False, z_axis=F
     # Compute the rotated uncertainty fields here so the time isn't added to the choice part of the function
 
     # Rotate around z axis by 45 degrees.
-    d1_rotation = rotate(uncertainty, [0,0,1], 45)
+    # d1_rotation = rotate(uncertainty, [0,0,1], 45)
+    d1_rotation, _, _ = true_img_rot(uncertainty, [0.707, 0.707, 0])
     # Rotate around z axis by -45 degrees.
-    d2_rotation = rotate(uncertainty, [0,0,1], 315)
+    d2_rotation = rotate(uncertainty, [0, 0, 1], 315)
     # Rotate around y axis by 45 degrees.
     d3_rotation = rotate(uncertainty, [0, 1, 0], 45)
     # Rotate around y axis by -45 degrees.
@@ -35,7 +37,7 @@ def discreet_get_optimal_slice(uncertainty, x_axis=False, y_axis=False, z_axis=F
     # Rotate around x axis by -45 degrees.
     d6_rotation = rotate(uncertainty, [1, 0, 0], 315)
 
-    #gradients = get_gradients(uncertainty)
+    # gradients = get_gradients(uncertainty)
     gradients = np.array([uncertainty, uncertainty, uncertainty])
     # print("gradients shape is {}".format(gradients.shape))
 
@@ -58,7 +60,7 @@ def discreet_get_optimal_slice(uncertainty, x_axis=False, y_axis=False, z_axis=F
             chosen_axis = "z"
             current_point = point
     if diagonal_1:
-        sum_t, point = find_maximal_x(np.array([d5_rotation, d5_rotation, d5_rotation]), show_plot)
+        sum_t, point = find_maximal_x(np.array([d1_rotation, d1_rotation, d1_rotation]), show_plot)
         if sum_t > current_maximum_uncertainity:
             current_maximum_uncertainity = sum_t
             chosen_axis = "d1"
@@ -71,7 +73,7 @@ def discreet_get_optimal_slice(uncertainty, x_axis=False, y_axis=False, z_axis=F
     elif chosen_axis == "z":
         return current_maximum_uncertainity, current_point, np.array([0, 0, 1]), "z"
     elif chosen_axis == "d1":
-        return current_maximum_uncertainity, current_point, np.array([0, 0, 1]), "d1"
+        return current_maximum_uncertainity, current_point, np.array([0.707, 0.707, 0]), "d1"
     # uncertainty, point, normal, chosen_axis
 
 

src/slice_select/rotation_methods.py
@@ -22,11 +22,24 @@ def pad_image(image):
     # Pad the image array using the smallest value as the constant
     padded_image = np.pad(image, pad_widths, mode='constant', constant_values=min_value)
 
-    return padded_image
+    padding_widths = pad_widths
+    original_shape = current_shape
 
+    return padded_image, pad_widths, original_shape
+
+def unpad_image(padded_image, pad_widths, original_shape):
+    # Calculate the indices for unpadding
+    unpad_indices = []
+    for i in range(len(original_shape)):
+        pad_width = pad_widths[i]
+        unpad_indices.append(slice(pad_width[0], -pad_width[1] or None))
+
+    # Unpad the image array using the calculated indices
+    unpadded_image = padded_image[(*unpad_indices, ...)]
+
+    return unpadded_image
 
 def rotate(image, axis, angle_deg, cval=0.0):
-    image = pad_image(image)
     min_value = np.min(image)
     # Define the rotation axis and angle
     axis = np.array(axis)  # Example: Rotate around the X-axis
@@ -143,6 +156,8 @@ def remove_padding(padded_image, pad_length):
 
 
 def true_img_rot(image, normal):
+    #print(image.shape)
+    image, pad_width, original_shape = pad_image(image)
     print(image.shape)
     x, y, z = image.shape
     m = min(x, min(y, z))
@@ -151,6 +166,7 @@ def true_img_rot(image, normal):
     rotations = find_rotation(normal)
     true_rotations = np.degrees(rotations)
     rotations = abs(true_rotations)
+    #print(true_rotations)
     rotim = image
     if rotations[0] > 0:
         rotim = rotate(rotim, [1, 0, 0], get_val(true_rotations[0]), cval=0.0)
@@ -160,17 +176,17 @@ def true_img_rot(image, normal):
         rotim = rotate(rotim, [0, 0, 1], get_val(true_rotations[2]), cval=0.0)
 
     #rotim = remove_padding(rotim, m)
-    return rotim
+    return rotim, pad_width, original_shape
 
 
-def true_img_rot_back(image, normal):
+def true_img_rot_back(image, normal, pad_width, original_shape):
     x, y, z = image.shape
     m = min(x, min(y, z))
     #image = add_padding(image, m)
     normal = np.array(normal).astype(float)
     rotations = find_rotation(normal)
     true_rotations = -1 * np.degrees(rotations)
-    print(true_rotations)
+    #print(true_rotations)
     rotations = abs(true_rotations)
     rotim = image
     if rotations[0] > 0:
@@ -179,8 +195,10 @@ def true_img_rot_back(image, normal):
         rotim = rotate(rotim, [0, 1, 0], get_val(true_rotations[1]), cval=0.0)
     if rotations[2] > 0:
         rotim = rotate(rotim, [0, 0, 1], get_val(true_rotations[2]), cval=0.0)
-
     #rotim = remove_padding(rotim, m)
+    #print(original_shape)
+    rotim = unpad_image(rotim, pad_width, original_shape)
+    #print(rotim.shape)
     return rotim
 
 # # Example usage